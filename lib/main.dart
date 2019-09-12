f143578fba7905f98d8b8c85f6cb82304f07635a
persilee <i@lishaoy.net>
2019-09-12 18:30:10 +0800

    🏓 modify main pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:ants/pages/business_page.dart';
+import 'package:ants/pages/home_page.dart';
+import 'package:ants/pages/my_page.dart';
 import 'package:flutter/material.dart';
 
 import 'navigator/tab_navigater.dart';
@@ -16,6 +19,11 @@ class MyApp extends StatelessWidget {
         highlightColor: Color.fromRGBO(255, 255, 255, 0.6),
         splashColor: Color.fromRGBO(244, 67, 54, 0.5),
       ),
+      routes: <String, WidgetBuilder>{
+        'home': (BuildContext context) => HomePage(),
+        'business': (BuildContext context) => BusinessPage(),
+        'my': (BuildContext context) => MyPage(),
+      },
       home: TabNavigator(),
     );
   }
